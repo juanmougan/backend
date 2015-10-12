f36b80b0f7d3133742171206338a417c1753960e
Juan Manuel Mougan <juanmougan@gmail.com>
2015-10-12 16:06:04 -0300

    Now inserts the Enrollment

--- app/jobs/csv_importer_job.rb
+++ app/jobs/csv_importer_job.rb
@@ -24,8 +24,11 @@ class CsvImporterJob < ActiveJob::Base
 	end_time = Time.now
 	puts "\n\n\n\n\n\Parse took: #{end_time - start_time} seconds"
 
-	# Store the Hash in the DB
-	# TODO refactor - extract to another method
+	store_all_students(student_hash)
+  end
+
+  def store_all_students(student_hash)
+  	# Store the Hash in the DB
 	ActiveRecord::Base.transaction do
 			student_hash.keys.each do |k|
 			some_student = student_hash[k]
@@ -34,15 +37,16 @@ class CsvImporterJob < ActiveJob::Base
 			student.first_name = some_student.first_name
 			student.last_name = some_student.last_name
 			student.file_number = some_student.file_number
-			# Query example: 		Subject.where(name: 'Ingeniería de Software II', id: 1)
-			# Ejemplo para Informática: 	Career.where(code: 11)
-			# flat_student.career = some_student.career				# TODO add Career, see: https://github.com/juanmougan/backend/issues/1
-			# student.career = Career.where(code: some_student.career_code)
 			student.career = Career.find_by code: some_student.career_code
-			# student.career = Career.where(code: some_student.career_code).first
-			puts student.save!	# TODO error handling here. Raise an exception?
+			student.save!
+			store_enrollments_for_student(student, some_student.subjects) 	# TODO error handling here. Raise an exception?
 		end
 	end
+  end
 
+  def store_enrollments_for_student(student, subjects)
+  	subjects.each do |subject|
+  		Enrollment.create(:year => Date.current.year, :student => student, :subject => subject)
+  	end
   end
 end
