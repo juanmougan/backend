43a25ef76fb5887bf3e5466809a2ca13f44f5cbe
Juan Manuel Mougan <jmougan@hexacta.com>
2015-10-16 11:35:09 -0300

    Added an inverse relationship

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,8 @@
 class Student < ActiveRecord::Base
   belongs_to :career
   has_many :notifications, through: :subscription_lists
+  
+  has_many :enrollments, inverse_of: :student
   has_many :subjects, through: :enrollments
+
 end
