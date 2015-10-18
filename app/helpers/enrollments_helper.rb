2b4b1fe7849c20ddf85ef6d1e6d5449c231459aa
Juan Manuel Mougan <juanmougan@gmail.com>
2015-10-18 17:44:34 -0300

    Added helper to get the student name

--- app/helpers/enrollments_helper.rb
+++ app/helpers/enrollments_helper.rb
@@ -1,2 +1,5 @@
 module EnrollmentsHelper
+  def get_student_full_name_from_enrollments(enrollments)
+  	"#{enrollments.first.student.last_name}, #{enrollments.first.student.first_name}" unless enrollments.first.nil?		
+  end
 end
