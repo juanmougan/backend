64792541680cc717cbdc65bec5fab5d2a13c25ab
Juan Manuel Mougan <jmougan@hexacta.com>
2015-10-21 10:50:15 -0300

    Improved notifications views

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -69,6 +69,6 @@ class NotificationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def notification_params
-      params[:notification]
+      params[:notification].permit(:title, :message, :subscription_list_id)
     end
 end
