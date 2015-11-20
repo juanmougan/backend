e289d34b9994aa6f1cf49416c33fcea1867a9336
Juan Manuel Mougan <juanmougan@gmail.com>
2015-11-19 21:57:59 -0300

    Removed debug code

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -9,16 +9,11 @@ class Api::V1::NotificationsController < ApplicationController
 
   # API only version
   def create
-    puts "\n\n\n\n\nHere are the params: #{params}\n\n\n\n\n"
-    puts "\n\n\n\n\nTitle: #{params[:title]}\n\n\n\n\n"
-    puts "\n\n\n\n\nMessage: #{params[:message]}\n\n\n\n\n"
-    puts "\n\n\n\n\nSL: #{params[:subscription_list]}\n\n\n\n\n"
     notification_params = Hash.new
     notification_params[:title] = params[:title]
     notification_params[:message] = params[:message]
     notification_params[:subscription_list] = SubscriptionList.find(params[:subscription_list])
     @notification = Notification.new(notification_params)
-    #@notification.subscription_list = SubscriptionList.find(params[:subscription_list])
 
     if @notification.save
       puts "\n\n\n\n\n\n\nWill respond with #{@notification}\n\n\n\n\n\n\n\n"
@@ -27,17 +22,6 @@ class Api::V1::NotificationsController < ApplicationController
       puts "\n\n\n\n\n\n\nWill respond with #{@notification.errors}\n\n\n\n\n\n\n\n"
       respond_with @notification.errors
     end
-
-=begin
-    respond_to do |format|
-      if @notification.save
-        #format.json { render :show, status: :created, location: @notification }
-        #render json: @notification
-      else
-        format.json { render json: @notification.errors, status: :unprocessable_entity }
-      end
-    end
-=end
   end
 
 end
