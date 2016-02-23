d02094bdc2abc0fcd59557b067c12399f4bb30a5
Juan Manuel Mougan <jmougan@hexacta.com>
2016-02-23 14:57:56 -0300

    Added some unit test, run them with rake test:units

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get_students_by_career_id" do
+  	students = Student.get_students_by_career_id(4)
+    assert_equal 1, students.size
+    assert_equal "00000001", students.first.file_number
+  end
 end
--- test/models/subscription_list_test.rb
+++ test/models/subscription_list_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class SubscriptionListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "retrieve_subject_subscription_lists" do
+    assert_equal 1, SubscriptionList.retrieve_subject_subscription_lists.size
+    assert_equal "Quimica General", SubscriptionList.retrieve_subject_subscription_lists.first.name
+  end
 end
